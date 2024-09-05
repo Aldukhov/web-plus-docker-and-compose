82d8e8148a92c73df4eae08722d26c6fceaa7564
Vasilii <vasia200257@gmail.com>
2024-09-05 17:33:19 +1000

    Change url to secure connection

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ import * as passport from 'passport';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://kupiproday.nomorepartiesco.ru',
+    origin: 'https://kupiproday.nomorepartiesco.ru',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
